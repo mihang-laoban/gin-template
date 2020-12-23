00821d6671df4da35dcb286ef53b6e58e358a936
程烨 <chengye1@xiaomi.com>
2020-12-23 22:18:37 +0800

    test

--- main.go
+++ main.go
@@ -16,5 +16,31 @@ func main() {
 		})
 	})
 
-	r.Run("x.tar:3000") // 监听并在 0.0.0.0:8080 上启动服务
+	createParams := []interface{}{
+		map[string]interface{}{
+			"key":     "1",
+			"name":    "John Brown",
+			"age":     32,
+			"address": "New York No. 1 Lake Park",
+			"tags":    []interface{}{"nice", "developer"},
+		}, map[string]interface{}{
+			"key":     "2",
+			"name":    "Jim Green",
+			"age":     42,
+			"address": "London No. 1 Lake Park",
+			"tags":    []interface{}{"loser"},
+		}, map[string]interface{}{
+			"key":     "3",
+			"name":    "Joe Black",
+			"age":     32,
+			"address": "Sidney No. 1 Lake Park",
+			"tags":    []interface{}{"cool", "teacher"},
+		},
+	}
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, createParams)
+	})
+
+	r.Run("x.tar:5000") // 监听并在 0.0.0.0:8080 上启动服务
 }
